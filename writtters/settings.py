78c5fdeb1feea0d0e6df4127906b6ec4bfaaa3a2
SusanMageto <susan.mageto@student.moringaschool.com>
2022-08-18 16:27:55 +0300

    deploy one

--- writtters/settings.py
+++ writtters/settings.py
@@ -80,12 +80,28 @@ WSGI_APPLICATION = 'writtters.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
+
+# PostgreSQL Database
+
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'da81ae82vg630j',
+        'USER': 'brjjlhepzorvfi',
+        'PASSWORD': '530cb9cc1c1b1ad2bec7e7c5eca99772078393413908dec598ab8a3e502985d2',
+        'HOST': 'ec2-54-85-56-210.compute-1.amazonaws.com',
+        'PORT': '5432',
     }
 }
+
 
 
 # Password validation
